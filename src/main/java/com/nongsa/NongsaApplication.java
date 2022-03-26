3c1544e2dc361ad6ce11abe0049d5937ae290970
moonstal1506 <moonstal1506@gmail.com>
2022-03-27 00:02:20 +0900

    환경세팅

---
+++ src/main/java/com/nongsa/NongsaApplication.java
@@ -0,0 +1,13 @@
+package com.nongsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NongsaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NongsaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nongsa/NongsaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nongsa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NongsaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
