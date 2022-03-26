d5e9a08b4de20489529fd1657b294992479c92e0
moonstal1506 <moonstal1506@gmail.com>
2022-03-27 04:00:52 +0900

    reply 테이블

--- src/main/java/com/nongsa/model/Board.java
+++ src/main/java/com/nongsa/model/Board.java
@@ -14,9 +14,18 @@ import javax.persistence.ManyToOne;
 
 import org.hibernate.annotations.CreationTimestamp;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 
 
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 public class Board {
 	
--- src/main/java/com/nongsa/model/Reply.java
+++ src/main/java/com/nongsa/model/Reply.java
@@ -1,5 +1,44 @@
 package com.nongsa.model;
 
-public class Reply {
+import java.sql.Timestamp;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+public class Reply {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id; 
+	
+	@Column(nullable=false,length=200)
+	private String content;
+	
+	@ManyToOne
+	@JoinColumn(name="boardId")
+	private Board board;
+	
+	@ManyToOne
+	@JoinColumn(name="userId")
+	private User user;
+	
+	
+	@CreationTimestamp
+	private Timestamp createDate;
 }
