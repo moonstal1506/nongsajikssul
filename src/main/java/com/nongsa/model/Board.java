10d11929c6f7595748ebe6cdec6dc18b0e24d94f
moonstal1506 <moonstal1506@gmail.com>
2022-03-27 03:53:59 +0900

    board 테이블

--- src/main/java/com/nongsa/model/Board.java
+++ src/main/java/com/nongsa/model/Board.java
@@ -1,5 +1,43 @@
 package com.nongsa.model;
 
-public class Board {
+import java.sql.Timestamp;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+
+
+
+@Entity
+public class Board {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(nullable=false, length=100)
+	private String title;
+	
+	@Lob
+	private String content;
+	
+	private int count;
+	
+	@ManyToOne(fetch =FetchType.EAGER)
+	@JoinColumn(name="userId")
+	private User user; 
+
+	
+	@CreationTimestamp
+	private Timestamp createDate;
 }
+
