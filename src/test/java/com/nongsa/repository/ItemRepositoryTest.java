41a31072860b1e73228cb018487779c432a693f3
moonstal1506 <moonstal1506@gmail.com>
2022-07-14 16:57:05 +0900

    build : Querydsl 의존성, 플러그인 추가

--- src/test/java/com/nongsa/repository/ItemRepositoryTest.java
+++ src/test/java/com/nongsa/repository/ItemRepositoryTest.java
@@ -2,17 +2,27 @@ package com.nongsa.repository;
 
 import com.nongsa.constant.ItemSellStatus;
 import com.nongsa.model.Item;
+import com.nongsa.model.QItem;
+import com.querydsl.jpa.impl.JPAQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
 @SpringBootTest
 class ItemRepositoryTest {
 
     @Autowired
     ItemRepository itemRepository;
+
+    @PersistenceContext
+    EntityManager em;
 
     @Test
     @DisplayName("상품 저장 테스트")
@@ -31,5 +41,31 @@ class ItemRepositoryTest {
 
         //then
         Assertions.assertThat(findItem.getItemName()).isEqualTo(item.getItemName());
+    }
+    public void createItemList(){
+        for(int i=1;i<=10;i++){
+            Item item = new Item();
+            item.setItemName("테스트 상품" + i);
+            item.setPrice(10000 + i);
+            item.setItemDetail("테스트 상품 상세 설명" + i);
+            item.setItemSellStatus(ItemSellStatus.SELL);
+            item.setStockNumber(100);
+            Item savedItem = itemRepository.save(item);
+        }
+    }
+
+    @Test
+    @DisplayName("Querydsl 조회 테스트")
+    public void queryDslTest(){
+        this.createItemList();
+        JPAQueryFactory queryFactory = new JPAQueryFactory(em);
+        QItem qItem = QItem.item;
+        JPAQuery<Item> query  = queryFactory.selectFrom(qItem)
+                .where(qItem.itemSellStatus.eq(ItemSellStatus.SELL))
+                .where(qItem.itemDetail.like("%" + "테스트 상품 상세 설명" + "%"))
+                .orderBy(qItem.price.desc());
+
+        List<Item> itemList = query.fetch();
+        Assertions.assertThat(itemList.get(0).getItemName()).isEqualTo("테스트 상품10");
     }
 }
